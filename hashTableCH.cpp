f5c3ba5d2fd2986139bb56f438bf3e597b820f2f
Mico-S <56495615+Mico-S@users.noreply.github.com>
2019-12-03 16:28:44 -0700

    Updated hashTableCH.cpp

--- hashTableCH.cpp
+++ hashTableCH.cpp
@@ -18,6 +18,42 @@ hashTableCH::hashTableCH(int bsize)
         table2[i] = -1;
     }
     numRecords = 0;
+    primeNumbers = new int[50];
+    createListOfPrimes(primeNumbers);
+    maxPossibleTableSize = primeNumbers[49];
+}
+
+// Function that creates an array of 50 prime numbers that are bigger than the given tableSize
+// (Populates data member "int* primeNumbers" array) 
+void hashTableCH::createListOfPrimes(int* arr)
+{
+    int numValuesInArr = 0;
+    int numPastTableSize = tableSize + 1;
+    bool isPrime;
+
+    // While there are less than 50 prime numbers in the primeNumbers array
+    while(numValuesInArr < 50)
+    {
+        isPrime = true;
+        // For loop that checks if a number is only divisible by 1 & itself
+        for(int i = 2; i <= numPastTableSize / 2; i++)
+        {
+            if(numPastTableSize % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        // If the number is prime, add it to the end of the array
+        if(isPrime)
+        {
+            arr[numValuesInArr] = numPastTableSize;
+            numValuesInArr++;
+        }
+        // Increment to the next number greater than the current tableSize
+        numPastTableSize++;
+    }
 }
 
 // Function to calculate index in the 1st hash table for a given key
@@ -58,6 +94,11 @@ int hashTableCH::searchItem(int key)
 // Inserts a key into hash table
 bool hashTableCH::insertItem(int key)
 {
+    if(tableSize == maxPossibleTableSize)
+    {
+        cout << "Sorry, cannot insert anymore values!" << endl;
+        return false;
+    }
     insertItemHelper(key, key, 0);
     return true;
 }
@@ -65,24 +106,15 @@ bool hashTableCH::insertItem(int key)
 // Inserts a key into hash table and does cuckoo hashing displacement recursively
 bool hashTableCH::insertItemHelper(int key, int originalKey, int counter)
 {
-    if(searchItem(key) != -1)
-    {
-        cout << "Value already exists!" << endl;
-        return false;
-    }
-
-    // If the key to be inserted is the same key as the original key that we were trying to insert and the counter > 0
-    // (meaning recursion has happened), then a cycle is occuring, so rehash the table 
+    bool successful;
+    // If the counter is > 0 (recursion has occured) & the displaced value we are trying to insert is the
+    // same as the original value we were trying to insert, then a cycle is occuring, so rehash the two tables
     if(key == originalKey && counter > 0)
     {
         insertedItems.push_back(key);
         cout << "Rehashing..." << endl;
         rehash();
-        // insertItemHelper(key, 0);
         return true;
-        // cout << "Changed size of tables to: " << tableSize << endl;
-        // cout << "Placement failed. Rehashing required" << endl;
-        // return false;
     }
 
     // Calculate indices using hash functions 1 & 2 and attempt
@@ -100,20 +132,16 @@ bool hashTableCH::insertItemHelper(int key, int originalKey, int counter)
 
     int displacedValue1 = table1[index];
     table1[index] = key; 
-    index = hashFunction2(displacedValue1);
-    
-    // If the key to be inserted is the same key as the original key that we were trying to insert and the counter > 0
-    // (meaning recursion has happened), then a cycle is occuring, so rehash the table 
+    index = hashFunction2(key);
+
+    // If the counter is > 0 (recursion has occured) & the displaced value we are trying to insert is the
+    // same as the original value we were trying to insert, then a cycle is occuring, so rehash the two tables
     if(displacedValue1 == originalKey && counter > 0)
     {
         insertedItems.push_back(key);
         cout << "Rehashing..." << endl;
         rehash();
-        // insertItemHelper(key, 0);
         return true;
-        // cout << "Changed size of tables to: " << tableSize << endl;
-        // cout << "Placement failed. Rehashing required" << endl;
-        // return false;
     }
 
     if(table2[index] == -1)
@@ -129,25 +157,32 @@ bool hashTableCH::insertItemHelper(int key, int originalKey, int counter)
     table2[index] = displacedValue1;
     counter++;
     insertItemHelper(displacedValue2, originalKey, counter);
-
-    // If both locations are full, then we displace the current value at the element
-    // from hash function 1 and store that displaced value, then replace the current element's
-    // value with the key value and recursively call insertItemHelper to attempt to insert the 
-    // displaced value
-    // index = hashFunction1(key);
-    // int displacedValue = table1[index];
-    // table1[index] = key;
-    // counter++;
-    // insertItemHelper(displacedValue, counter);
 }
 
 // Rehashes (rebuilds) the current hash table with a new size
 void hashTableCH::rehash()
 {
-    // Declare a new hash table
-    // srand(time(NULL));
-    // int randNum = rand() % tableSize + 1; 
-    tableSize = tableSize * 2;
+    int oldTableSize = tableSize;
+    // Make the new tableSize a prime number that is larger than the current tableSize
+    for(int i = 0; i < 50; i++)
+    {
+        if(primeNumbers[i] > tableSize)
+        {
+            tableSize = primeNumbers[i];
+            cout << "New Table Size: " << tableSize << endl;
+            break;
+        }
+    }
+    bool successful;
+    
+    // If the tableSize did not change (meaning that all 50 prime numbers have been used to change the tableSize),
+    // then we cannot rehash any further and have reached the max load factor
+    if(oldTableSize == tableSize)
+    {
+        cout << "Cannot re-hash further. Max Load Factor hit for this data set." << endl;
+        return;
+    }
+
     int* newTable1 = new int[tableSize];
     int* newTable2 = new int[tableSize];
     for(int i = 0; i < tableSize; i++)
@@ -156,44 +191,41 @@ void hashTableCH::rehash()
         newTable2[i] = -1;
     }
 
-    // Place all the values from the current hash table into the new hash table
-    // for(int i = 0; i < tableSize; i++)
-    // {
-    //     newTable1[i] = table1[i];
-    //     newTable2[i] = table2[i];
-    // }
-    // for(int i = 0; i < tableSize - 175; i++)
-    // {
-    //     if(table1[i] != -1)
-    //     {
-    //         rehashHelper(newTable1, newTable2, table1[i], 0);  
-    //     }
-    //     else if(table2[i] != -1)
-    //     {
-    //         rehashHelper(newTable1, newTable2, table2[i], 0);
-    //     }
-    // }
+    // Re-hashing all the previously inserted values into the new tables
     for(int j = 0; j < insertedItems.size(); j++)
     {
-        rehashHelper(newTable1, newTable2, insertedItems[j], 0);
+        cout << "Attempting to re-insert value: " << insertedItems[j] << " --- ";
+        successful = rehashHelper(newTable1, newTable2, insertedItems[j], insertedItems[j], 0);
+        if(successful)
+        {
+            cout << "Re-Inserted Value: " << insertedItems[j] << endl;
+        }
+        else
+        {
+            return;
+        }
     }
 
+    // Deallocating table1 & table2, then make table1 & table2 point to the new tables
     delete [] table1;
     delete [] table2;
     table1 = newTable1;
     table2 = newTable2;
 }
 
-bool hashTableCH::rehashHelper(int* newTable1, int* newTable2, int key, int counter)
+bool hashTableCH::rehashHelper(int* newTable1, int* newTable2, int key, int originalKey, int counter)
 {
-    if(counter >= tableSize)
+    // If the counter is > 0 (recursion has occured) & the displaced value we are trying to insert is the
+    // same as the original value we were trying to insert, then a cycle is occuring, so rehash the two tables 
+    if(key == originalKey && counter > 0)
     {
-        // cout << "Rehashing of " << key << " failed." << endl;
-        // return false;
-        // rehash();
-        counter = 0;
+        cout << "Re-Rehashing..." << endl;
+        delete [] newTable1;
+        delete [] newTable2;
+        rehash();
         return false;
     }
+
     // Calculate indices using hash functions 1 & 2 and attempt
     // to insert the key value into a slot
     int index = hashFunction1(key);
@@ -209,6 +241,17 @@ bool hashTableCH::rehashHelper(int* newTable1, int* newTable2, int key, int coun
     newTable1[index] = key; 
     index = hashFunction2(displacedValue1);
 
+    // If the counter is > 0 (recursion has occured) & the displaced value we are trying to insert is the
+    // same as the original value we were trying to insert, then a cycle is occuring, so rehash the two tables 
+    if(displacedValue1 == originalKey && counter > 0)
+    {
+        cout << "Re-Rehashing..." << endl;
+        delete [] newTable1;
+        delete [] newTable2;
+        rehash();
+        return false;
+    }
+
     if(newTable2[index] == -1)
     {
         // cout << "Inserting: " << displaced << " with hash function 2" << endl;
@@ -219,7 +262,7 @@ bool hashTableCH::rehashHelper(int* newTable1, int* newTable2, int key, int coun
     int displacedValue2 = newTable2[index];
     newTable2[index] = displacedValue1;
     counter++;
-    rehashHelper(newTable1, newTable2, displacedValue2, counter);
+    rehashHelper(newTable1, newTable2, displacedValue2, originalKey, counter);
 }
 
 // Deletes a given key value from the hash table
@@ -258,11 +301,11 @@ void hashTableCH::printTable()
         cout << i <<"|| ";
         if(table1[i] != -1)
         {
-            cout << table1[i];
+            cout << table1[i] << " ";
         }
         else
         {
-            cout << "------";
+            cout << "------ ";
         }
         if(table2[i] != -1)
         {
